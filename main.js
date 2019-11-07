923926b6e9f5eaba0c8b4fc141d9401126804632
JHC <38774190+JuanHoyosC@users.noreply.github.com>
2019-11-06 23:49:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const Block = require('./block');
-const BlockChain = require('./blockChain');
+const Block = require('bloque.js');
+const BlockChain = require('blockChain.js');
 
 //Inicializa blockchain
 let bloque = new BlockChain();
 //Crean un nuevo bloque
 bloque.agregarBloque(new Block('01/01/2018', { cantidad: 45 }));
-bloque.agregarBloque(new Block('03/10/2018', { cantidad: 60 }));
-bloque.agregarBloque(new Block('04/11/2018', { cantidad: 34 }));
+bloque.agregarBloque(new Block('01/01/2019', { cantidad: 5 }));
 //Muestra la cadena de bloque
+
 console.log(JSON.stringify(bloque, null, 4));
+console.log(bloque.validarChain());
+bloque.chain[2].datos = {cantidad:47};
+console.log(bloque.validarChain());
